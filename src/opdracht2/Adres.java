b6886a4602037a2eb3d26093d300f02fd2cfbca4
XerenaJue <xerenajue@hotmail.com>
2016-05-09 15:10:46 +0200

    test

---
+++ src/opdracht2/Adres.java
@@ -0,0 +1,52 @@
+package opdracht2;
+
+public class Adres {
+    
+    private String straatnaam;
+    private String postcode;
+    private String toevoeging;
+    private int huisnummer;
+    private String woonplaats;
+    
+    public Adres() {}
+    
+    public String getStraatnaam() {
+        return straatnaam;        
+    }
+    
+    public void setStraatnaam(String straatnaam) {
+        this.straatnaam = straatnaam;
+    }
+    
+    public String getPostcode() {
+        return postcode;
+    }
+    
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+    
+    public String getToevoeging() {
+        return toevoeging;
+    }
+    
+    public void setToevoeging(String toevoeging) {
+        this.toevoeging = toevoeging;
+    }
+    
+    public int getHuisnummer() {
+        return huisnummer;
+    }
+    
+    public void setHuisnummer(int huisnummer) {
+        this.huisnummer = huisnummer;
+    }
+    
+    public String getWoonplaats() {
+        return woonplaats;
+    }
+    
+    public void setWoonplaats(String woonplaats) {
+        this.woonplaats = woonplaats;
+    }
+}
