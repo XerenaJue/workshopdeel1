865265ad10553668559899280cb1fc0c5ad81582
jeroen <jeroen@jeroen-OEM>
2016-05-10 10:21:10 +0200

    testing locally

---
+++ WorkshopDeel1/src/ConnectionFactory.java
@@ -0,0 +1,50 @@
+package nl.rsvier;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+ 
+
+/* from theopentutorials.com
+ 
+ */
+
+
+/**
+ *
+ * @author jeroen
+ */
+
+
+ 
+public class ConnectionFactory {
+    //static reference to itself
+    private static ConnectionFactory instance = new ConnectionFactory();
+    public static final String URL = "jdbc:mysql://localhost/workshopdeel1";
+    public static final String USER = "hallo";
+    public static final String PASSWORD = "doei";
+    public static final String DRIVER_CLASS = "com.mysql.jdbc.Driver"; 
+     
+    //private constructor
+    private ConnectionFactory() {
+        try {
+            Class.forName(DRIVER_CLASS);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+     
+    private Connection createConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("ERROR: Unable to Connect to Database.");
+        }
+        return connection;
+    }   
+     
+    public static Connection getMySQLConnection() {
+        return instance.createConnection();
+    }
+}
---
+++ WorkshopDeel1/src/TestArtikel1.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.rsvier;
+
+/**
+ *
+ * @author jeroen
+ */
+public class TestArtikel1 {
+    public static void main(String[] args) {
+        ArtikelPOJO art = new ArtikelPOJO();
+        ArtikelDAO artDAO = new ArtikelDAO(art);
+        // leeg artikel
+        System.out.println(art);
+        
+        art = artDAO.readArtikel(1);
+        // print zoals nu in database bij besetellingID 1
+        System.out.println(art);
+        
+        artDAO.updateArtikel("pot", 1);    
+        art = artDAO.readArtikel(1);
+        // print aangepaste
+        System.out.println(art);
+        
+        artDAO.updateArtikel("urinoir", 1);
+        art = artDAO.readArtikel(1);
+        // enkel de naam van artikel in bestelling 1 is aangepast
+        System.out.println(art);
+        
+        art = new ArtikelPOJO();
+        artDAO = new ArtikelDAO(art);
+        
+        art.setArtikelID(1);
+        art.setArtikelNaam("bankstel");
+        art.setArtikelPrijs(1000);
+        // hieronder wordt de hele pojo in 1 keer in tabel gezet
+        artDAO.updateArtikel(1); 
+        
+        ArtikelPOJO nogEenArt = new ArtikelPOJO();
+        ArtikelDAO nogEenArtDAO = new ArtikelDAO(nogEenArt);
+       
+        nogEenArt = nogEenArtDAO.readArtikel(1);
+        // nieuwe pojo wordt uitgeprint 
+        System.out.println(nogEenArt);
+        
+    }
+}
