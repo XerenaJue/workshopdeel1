c33f5d5202da86b5dbc9a93ca6a060b627178ba6
jeroen <jeroen@jeroen-OEM>
2016-05-13 23:34:32 +0200

    kan nu (als bestellingDAO werkt) op algemene aanvraag antwoorden met een  toDisplay-array met Klant, Adres, Bestelling, lijstje artikelen.. zie kleine test: zou moeten werken met autogevulde database

--- src/facade/testDeFacade.java
+++ src/facade/testDeFacade.java
@@ -12,7 +12,7 @@ import opdracht2.*;
 public class testDeFacade {
     public static void main(String[] args)  throws Exception {
         FacadeDatabaseMenu facade = new FacadeDatabaseMenu();
-         
+        FillBatchDatabase.fillBatchDatabase();
         Object[] nepAppArray = facade.getToDisplay();
         Klant klant = new Klant(); 
         nepAppArray[0] = klant;
