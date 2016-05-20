e4868bbfd6335ab189cdb7963380ffe59d1f6841
maurice <maurice@lolcat>
2016-05-20 18:23:39 +0200

    een geteste en werkende BestellingDAO

--- src/opdracht2/BestellingDAO.java
+++ src/opdracht2/BestellingDAO.java
@@ -31,23 +31,18 @@ public class BestellingDAO{
 
     //overloaded getter method voor klant_id
     public Bestelling readBestelling(int klant_id) throws SQLException{
-        System.out.println("geen resultset");
         Bestelling bestelling = new Bestelling();
         String SQLStatement = "SELECT * FROM bestelling WHERE bestelling_id =" + klant_id;
         try{
         createCS(SQLStatement);
         //statement.setInt(1, klant_id);
         resultSet = statement.executeQuery();
-        System.out.println(connection.toString());
         while(resultSet.next()){
             bestelling.setBestellingID(resultSet.getInt("bestelling_id"));
-            System.out.println("resultsetgetint: " + resultSet.getInt("bestelling_id"));
             bestelling.setKlant(resultSet.getInt("klant_id"));
             bestelling.setAantalArtikel1(resultSet.getInt("artikel_aantal"));
-            System.out.println("artikel_aantal: " + resultSet.getInt("artikel_aantal"));
             bestelling.setAantalArtikel2(resultSet.getInt("artikel1_aantal"));
             bestelling.setAantalArtikel3(resultSet.getInt("artikel2_aantal"));
-            System.out.println(bestelling.toString());
         }
         }
         catch (SQLException e){}
@@ -90,38 +85,22 @@ public class BestellingDAO{
        // }
     //}
     
-    public void createBestelling(Klant klant)throws SQLException{
+    public void createBestelling(Bestelling bestelling)throws SQLException{
         try {
-           String SQLStatement = "UPDATE bestelling SET bestelling_id = ?, artikel_aantal = ?, artikel2_aantal = ?, artikel3_aantal = ? WHERE klant_id = ?";
-
-            Bestelling bestelling = new Bestelling();
-           bestelling.setAantalArtikel1(1);
-           bestelling.setAantalArtikel2(2);
-           bestelling.setAantalArtikel3(3);
-
+           String SQLStatement = "insert into bestelling (klant_id, artikel_aantal, artikel2_aantal, artikel3_aantal) VALUES (?,?,?,?)";
             connection = ConnectionFactory.getMySQLConnection();
             statement = connection.prepareStatement(SQLStatement, Statement.RETURN_GENERATED_KEYS);
-            //wijs door database gegenereerde id toe aan bestelling
+            statement.setInt(1, bestelling.getKlant_id());
+            statement.setInt(2, bestelling.getArtikel_aantal());
+            statement.setInt(3, bestelling.getAantalArtikel2());
+            statement.setInt(4, bestelling.getAantalArtikel3());
+            statement.executeUpdate();
             resultSet = statement.getGeneratedKeys();
             if (resultSet.isBeforeFirst()) {
                 resultSet.next();
                 bestelling.setBestellingID(resultSet.getInt(1));
             }
-                   System.out.println(bestelling.getBestelling_id());
-            statement.setInt(1, bestelling.getBestelling_id());
-            statement.setInt(2, klant.getKlantID());
-            System.out.println(klant.getKlantID());
-                   System.out.println(bestelling.getArtikel_aantal());
-            statement.setInt(3, bestelling.getArtikel_aantal());
-            statement.setInt(4, bestelling.getAantalArtikel2());
-            statement.setInt(5, bestelling.getAantalArtikel3());
-            statement.executeUpdate();
-            
-            
-            
-
-            
-            
+ 
 
         }
             catch(SQLException ex){}
@@ -133,8 +112,7 @@ public class BestellingDAO{
     
     //overloaded setter method voor klant_id
     public void updateBestelling(Bestelling bestelling) throws SQLException{
-           String SQLStatement = 
-                   "update bestelling set artikel_aantal = ?, artikel2_aantal = ?, artikel3_aantal = ? where bestelling_id = ?"; 
+           String SQLStatement ="update bestelling set artikel_aantal = ?, artikel2_aantal = ?, artikel3_aantal = ? where bestelling_id = ?";
         try {
             createCS(SQLStatement);
             statement.setInt(1,bestelling.getArtikel_aantal());
@@ -167,20 +145,6 @@ public class BestellingDAO{
             
         
     }
-    public static void main(String args[]) throws SQLException, ClassNotFoundException{
-        BestellingDAO test = new BestellingDAO();
-        Klant klant = new Klant();
-        klant.setKlantID(15);
-        test.createBestelling(klant);
-        //Bestelling bestelling = new Bestelling();
-        //bestelling.setBestellingID(16);
-        //test.deleteBestelling(bestelling);
-        
-        //Bestelling bestelling = test.readBestelling(3);
-        //System.out.println("BestellingsID: " + bestelling.getBestelling_id());
-        //System.out.println("Artikel aantal: " + bestelling.getArtikel_aantal());
-         //       System.out.println("tostring"+ bestelling.toString());
-         //           System.out.println("blablablabla");
-    }
+
             
 }
