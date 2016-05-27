f482fecaf285d574567fed528287085d03d9864f
maurice <maurice@lolcat>
2016-05-27 15:39:36 +0200

    POJO met getters en setters voor een ArtikelPOJO en aantal_artikelen column

---
+++ src/opdracht2/ArtikelBestelling.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opdracht2;
+
+/**
+ *
+ * @author maurice
+ */
+public class ArtikelBestelling {
+    private ArtikelPOJO artikel;
+    private int aantal_artikelen;
+    
+    public ArtikelPOJO getArtikelPojo(){
+        return artikel;
+    }
+
+    public void setArtikelPojo(ArtikelPOJO artikel){
+        this.artikel = artikel;
+    }
+    
+    public int getArtikelenAantal(){
+        return aantal_artikelen;
+    }
+    
+    public void setArtikelenAantal(int aantal_artikelen){
+        this.aantal_artikelen = aantal_artikelen;
+    }
+    
+    @Override
+    public String toString(){
+        return "ArtikelBestellingPOJO: " + artikel.toString() + ". aantal artikelen: " + aantal_artikelen; 
+    }
+}
